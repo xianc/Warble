4ed74217e0aadc29fc6a8feb3100d78e8a91a523
rwbecker <rbecker@student.umass.edu>
2013-04-03 20:29:25 -0400

    diffcomments

--- routes/user-after.js
+++ routes/user-after.js
@@ -1,103 +1,103 @@
 // # user-after.js
 var user = require('../lib/user');
 
-// ### Records the user logged in
+// Records the user logged in
 var userids = 0;
-// ### Records all users that are logged in in a "database"
+// Records all users that are logged in in a "database"
 var online = {};
 
-// ## Flash
+// ### Flash
 
 function flash(req, res, name, value) {
-  // ### If `value` is not undefined we are *setting* a flash
-  // ### value (i.e., setting a cookie).
+  // If `value` is not undefined we are *setting* a flash
+  // value (i.e., setting a cookie).
   if (value !== undefined) {
     res.cookie(name, value);
-    // ### We return the `value` to be consistent with the
-    // ### alternative call - to retrieve the value.
+    // We return the `value` to be consistent with the
+    // alternative call - to retrieve the value.
     return value;
   }
   else {
-    // ### Grab the `value` from the cookies sent along with
-    // ### the request.
+    // Grab the `value` from the cookies sent along with
+    // the request.
     value = req.cookies[name];
-    // ### Clear the cookie in the response.
+    // Clear the cookie in the response.
     res.clearCookie(name);
-    // ### Return the `value`.
+    // Return the `value`.
     return value;
   }
 }
 
-// # User Server-Side Routes
+// ## User Server-Side Routes
 
-// ## login
+// ### login
 // Provides a user login view.
 exports.login = function(req, res){
-  // ### Grab any messages being sent to use from redirect.
+  // Grab any messages being sent to use from redirect.
   var authmessage = flash(req, res, 'auth') || '';
 
-  // ### TDR: redirect if logged in:
+  // TDR: redirect if logged in:
   var userid  = req.cookies.userid;
 
-  // ### TDR: If the user is already logged in - we redirect to the
-  // ### main application view. We must check both that the `userid`
-  // ### and the `online[userid]` are undefined. The reason is that
-  // ###the cookie may still be stored on the client even if the
-  // ### server has been restarted.
+  // TDR: If the user is already logged in - we redirect to the
+  // main application view. We must check both that the `userid`
+  // and the `online[userid]` are undefined. The reason is that
+  // the cookie may still be stored on the client even if the
+  // server has been restarted.
   if (userid !== undefined && online[userid] !== undefined) {
     res.redirect('/user/main');
   }
   else {
-    // ### Render the login view if this is a new login.
+    // Render the login view if this is a new login.
     res.render('login', { title   : 'User Login',
                           message : authmessage });
   }
 };
 
-// ## auth
-// ### Performs **basic** user authentication.
+// ### auth
+// Performs **basic** user authentication.
 exports.auth = function(req, res) {
-  // ### TDR: redirect if logged in:
+  // TDR: redirect if logged in:
   var userid = req.cookies.userid;
 
-  // ### TDR: do the check as described in the `exports.login` function.
+  // TDR: do the check as described in the `exports.login` function.
   if (userid !== undefined && online[userid] !== undefined) {
     res.redirect('/user/main');
   }
   else {
-    // ### Pull the values from the form.
+    // Pull the values from the form.
     var username = req.body.username;
     var password = req.body.password;
-    // ### Perform the user lookup.
+    // Perform the user lookup.
     user.lookup(username, password, function(error, user) {
       if (error) {
-        // ### If there is an error we "flash" a message to the
-        // ### redirected route `/user/login`.
+        // If there is an error we "flash" a message to the
+        // redirected route `/user/login`.
         flash(req, res, 'auth', error);
         res.redirect('/user/login');
       }
       else {
-        // ### TDR: use cookie to record stateful connection. Here
-        // ### we record the generated userid as a cookie to be
-        // ### passed back and forth between client and server.
+        // TDR: use cookie to record stateful connection. Here
+        // we record the generated userid as a cookie to be
+        // passed back and forth between client and server.
         userid = ++userids;
         res.cookie('userid',
                    userid+'',
                    { maxAge : 900000 }); // 15 minutes
 
-        // ### Store the user in our in memory database.
+        // Store the user in our in memory database.
         online[userid] = user;
-        // ### Redirect to main.
+        // Redirect to main.
         res.redirect('/user/main');
       }
     });
   }
 };
 
-// ## logout
-// ### Deletes user info & cookies - then redirects to login.
+// ### logout
+// Deletes user info & cookies - then redirects to login.
 exports.logout = function(req, res) {
-  // ### TDR: handle cookies
+  // TDR: handle cookies
   var userid = req.cookies.userid;
   if (online[userid] !== undefined) {
     res.clearCookie('userid');
@@ -106,10 +106,10 @@ exports.logout = function(req, res) {
   res.redirect('/');
 };
 
-// ## main
-// ### The main user view.
+// ### main
+// The main user view.
 exports.main = function(req, res) {
-  // ### TDR: added cookie support
+  // TDR: added cookie support
   var userid = req.cookies.userid;
   if (userid === undefined || online[userid] === undefined) {
     flash(req, res, 'auth', 'Not logged in!');
@@ -128,7 +128,7 @@ exports.main = function(req, res) {
   }
 };
 
-// ## shows the other users online at a point in time
+// shows the other users online at a point in time
 exports.online = function(req, res) {
   var users = online[userid];
   res.render('online', { title : 'user Online',
@@ -141,13 +141,13 @@ exports.warbles = function(req, res) {
                          users : online });
 };
 
-// ## lets users discover other users or Warbles
+// lets users discover other users or Warbles
 exports.discover = function (req,res) {
   res.render('discover', { title  : 'Discover',
                             users : online,
                             allUsers : user.getUserdb()});
 }
-// ## shows Warbles at a user
+// shows Warbles at a user
 exports.me = function (req, res) {
   var userid = req.cookies.userid;
   if (userid === undefined || online[userid] === undefined) {
@@ -161,15 +161,15 @@ exports.me = function (req, res) {
                     warble : user.getWarbledb()  });
 }
 }
-// ## renders about page
+// renders about page
 exports.about = function (req, res) {
   res.render('about', { title  : 'About'});
 }
-// ## allows for uploads
+// allows for uploads
 exports.upload = function (req, res) {
   res.render('upload', { title  : 'Upload'});
 }
-// ## allows for users and warbles to be displayed on profile page
+// allows for users and warbles to be displayed on profile page
 exports.my_profile = function (req, res) {
    var userid = req.cookies.userid;
   if (userid === undefined || online[userid] === undefined) {
@@ -186,7 +186,7 @@ exports.my_profile = function (req, res) {
 							  });
   }
 }
-// ## gets the users following the logged in Warbler
+// gets the users following the logged in Warbler
 exports.followers = function (req, res) {
    var userid = req.cookies.userid;
   if (userid === undefined || online[userid] === undefined) {
@@ -202,7 +202,7 @@ exports.followers = function (req, res) {
 							});
   }
 }
-// ## gets the users a Warbler is following
+// gets the users a Warbler is following
 exports.following = function (req, res) {
    var userid = req.cookies.userid;
   if (userid === undefined || online[userid] === undefined) {
@@ -218,7 +218,7 @@ exports.following = function (req, res) {
 							});
   }
 }
-// ## creates user list
+// creates user list
 exports.form = function (req, res) {
   var id = req.params.id;
   genUserList(function (ul) {
@@ -230,7 +230,7 @@ exports.form = function (req, res) {
   });
 };
 
-//###Processes form get requests:
+// Processes form get requests:
 exports.process = function (req, res) {
   var id   = req.params.id;
   var auser = userData(req);
@@ -266,7 +266,7 @@ exports.process = function (req, res) {
     });
   }
 };
-// ## checks for user
+// checks for user
 function userData(req) {
   var auser;
   if (req.method === 'GET') {
@@ -302,7 +302,7 @@ function userData(req) {
 }
 
 
-//###Displays user
+// Displays user
 function genUserList(callback) {
   var i;
   user.getUserInfo([], function (list) {
