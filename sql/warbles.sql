5385e404f6b7d10b9ae0394ccea0efafa99592bb
HRIDYA TURLAPATI <hridya.turlapati@gmail.com>
2013-04-24 17:08:19 -0400

    Warbles sql changes

--- sql/warbles.sql
+++ sql/warbles.sql
@@ -11,13 +11,13 @@ create table users (
 	uid integer primary key autoincrement,
 	username varchar(50) not null,
 	password varchar(8) not null,
-	birthday varchar(10)
+	birthday varchar(10) null
 	);
 	
 create table warbles (
   id integer primary key autoincrement,
   username varchar(50) not null,
-  when date not null,
+  day date not null,
   message varchar(350) not null
 --  attachment varchar (60),
 --  warbleAt varchar(50),
@@ -33,26 +33,39 @@ create table followers (
 
 insert into users values
   (01, 'Xian', 'Chocolate', '07/19/92'),
-  (02, 'Eric', 'Smith!', '');
-  (03, 'Tim', 'cs326', 'N/A');
+  (02, 'Eric', 'Smith!', ''),
+  (03, 'Tim', 'warble', 'N/A'),
+  (04, 'Hridya', 'Puppies', '11/04/93'),
+  (05, 'Ryan', 'CS326', '09/04/91'),
+  (06, 'Michelle', 'qwerty', '08/03/91');
 
 insert into warbles values
   (1,'Xian','4/21/13 at 2:15pm','shtarbucks refreshhherrssss nonomnomz'),
   (2,'Xian','4/22/13 at 2:28pm','daaaaaataaabasseeeee'),
   (3,'Eric','4/23/13 at 4:50pm','yolo!'),
-  (4,'Xian','4/23/13 at 12:02pm','wanna sshleeeppp alllll dayyyy');
+  (4,'Xian','4/23/13 at 12:02pm','wanna sshleeeppp alllll dayyyy'),
+  (5,'Hridya','4/24/13 at 12:02pm','Upload worksss!!!'),
+  (6,'Hridya','4/24/13 at 12:05pm','The weather is FINALLY getting better!'),
+  (7,'Tim','4/23/13 at 12:10pm','Semester ends on May 1st'),
+  (8,'Michelle','4/22/13 at 12:20pm','Warbling is cool!');
 
 insert into followers values
   ('Xian','Eric'),
   ('Tim', 'Xian'),
-  ('Tim', 'Eric');
+  ('Tim', 'Eric'),
+  ('Hridya','Xian'),
+  ('Ryan','Eric'),
+  ('Ryan','Hridya'),
+  ('Michelle','Hridya'),
+  ('Michelle','Xian');
+
 
 -- simple select
 select * from warbles;
 
 -- select particular attributes:
 select username from warbles;
-order by id;
+-- order by id;
 
 -- select with condition
 /*select sname, rating, age
